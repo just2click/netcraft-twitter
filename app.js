070d3204404cae0c08487dfb110f5aa29bda4043
Dror <just2click@outlook.com>
2017-04-26 14:56:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require("./server/routes");
 
 // Start the server
-var server = app.listen(8000, function() {
+var server = app.listen(8002, function() {
  console.log('Listening on port %d', server.address().port);
 });
